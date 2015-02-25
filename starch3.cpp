0ea9f060db491310c99b4eaca3300bd8766ead9f
Alex Reynolds <alexpreynolds@gmail.com>
2015-02-25 15:20:04 -0800

    adding starch namespace support

--- starch3.cpp
+++ starch3.cpp
@@ -1,18 +1,20 @@
 #include "starch3.hpp"
 
-const std::string starch::Starch::general_name = "starch3";
-const std::string starch::Starch::version = S3_VERSION;
-const std::string starch::Starch::authors = "Alex Reynolds";
-const std::string starch::Starch::general_usage = "\n"                  \
+using namespace starch;
+
+const std::string Starch::general_name = "starch3";
+const std::string Starch::version = S3_VERSION;
+const std::string Starch::authors = "Alex Reynolds";
+const std::string Starch::general_usage = "\n"                  \
     "  Usage:\n"                                                        \
     "\n"                                                                \
     "  $ starch3 [options] < input > output\n";
-const std::string starch::Starch::general_description =                 \
+const std::string Starch::general_description =                 \
     "  Compress sorted BED data to BEDOPS Starch archive format.\n";
-const std::string starch::Starch::general_io_options = "";
-const std::string starch::Starch::general_options = "";
-const std::string starch::Starch::client_opt_string = "hv?";
-struct option starch::Starch::client_long_options[] = {
+const std::string Starch::general_io_options = "";
+const std::string Starch::general_options = "";
+const std::string Starch::client_opt_string = "hv?";
+struct option Starch::client_long_options[] = {
     { "help",           no_argument,         NULL,    'h' },
     { "version",        no_argument,         NULL,    'w' },
     { NULL,             no_argument,         NULL,     0  }
@@ -20,23 +22,23 @@ struct option starch::Starch::client_long_options[] = {
 
 int main(int argc, char** argv) 
 {
-    starch::Starch::init_command_line_options(argc, argv);
+    Starch::init_command_line_options(argc, argv);
 
     return EXIT_SUCCESS;
 }
 
 void
-starch::Starch::init_command_line_options(int argc, char **argv)
+Starch::init_command_line_options(int argc, char **argv)
 {
 #ifdef DEBUG
-    fprintf(stderr, "--- starch::Starch::init_command_line_options() - enter ---\n");
+    fprintf(stderr, "--- Starch::init_command_line_options() - enter ---\n");
 #endif
 
     int client_long_index;
     int client_opt = getopt_long(argc,
                                  argv,
-                                 starch::Starch::client_opt_string.c_str(),
-                                 starch::Starch::client_long_options,
+                                 Starch::client_opt_string.c_str(),
+                                 Starch::client_long_options,
                                  &client_long_index);
 
     opterr = 0; /* disable error reporting by GNU getopt */
@@ -45,27 +47,27 @@ starch::Starch::init_command_line_options(int argc, char **argv)
         switch (client_opt) 
             {
             case 'h':
-                starch::Starch::print_usage(stdout);
+                Starch::print_usage(stdout);
                 exit(EXIT_SUCCESS);
             case 'v':
-                starch::Starch::print_version(stdout);
+                Starch::print_version(stdout);
                 exit(EXIT_SUCCESS);
             case '?':
-                starch::Starch::print_usage(stdout);
+                Starch::print_usage(stdout);
                 exit(EXIT_SUCCESS);
             }        
     }
 
 #ifdef DEBUG
-    fprintf(stderr, "--- starch::Starch::init_command_line_options() - exit  ---\n");
+    fprintf(stderr, "--- Starch::init_command_line_options() - exit  ---\n");
 #endif
 }
 
 void
-starch::Starch::print_usage(FILE *stream)
+Starch::print_usage(FILE *stream)
 {
 #ifdef DEBUG
-    fprintf(stderr, "--- starch::Starch::print_usage() - enter ---\n");
+    fprintf(stderr, "--- Starch::print_usage() - enter ---\n");
 #endif
 
     fprintf(stream,
@@ -76,35 +78,35 @@ starch::Starch::print_usage(FILE *stream)
             "%s\n"            \
             "%s\n"            \
             "%s\n",
-            starch::Starch::general_name.c_str(),
-            starch::Starch::version.c_str(),
-            starch::Starch::authors.c_str(),
-            starch::Starch::general_usage.c_str(),
-            starch::Starch::general_description.c_str(),
-            starch::Starch::general_io_options.c_str(), 
-            starch::Starch::general_options.c_str());
+            Starch::general_name.c_str(),
+            Starch::version.c_str(),
+            Starch::authors.c_str(),
+            Starch::general_usage.c_str(),
+            Starch::general_description.c_str(),
+            Starch::general_io_options.c_str(), 
+            Starch::general_options.c_str());
 
 #ifdef DEBUG
-    fprintf(stderr, "--- starch::Starch::print_usage() - exit  ---\n");
+    fprintf(stderr, "--- Starch::print_usage() - exit  ---\n");
 #endif
 }
 
 void
-starch::Starch::print_version(FILE *stream)
+Starch::print_version(FILE *stream)
 {
 #ifdef DEBUG
-    fprintf(stderr, "--- starch::Starch::print_version() - enter ---\n");
+    fprintf(stderr, "--- Starch::print_version() - enter ---\n");
 #endif
 
     fprintf(stream,
             "%s\n"            \
             "  version: %s\n" \
             "  author:  %s\n",
-            starch::Starch::general_name.c_str(),
-            starch::Starch::version.c_str(),
-            starch::Starch::authors.c_str());
+            Starch::general_name.c_str(),
+            Starch::version.c_str(),
+            Starch::authors.c_str());
 
 #ifdef DEBUG
-    fprintf(stderr, "--- starch::Starch::print_version() - exit  ---\n");
+    fprintf(stderr, "--- Starch::print_version() - exit  ---\n");
 #endif
 }
